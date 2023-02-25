11a0b614923350f9cbb676ddba9d7dc37016dbde
Ricardo <83619661+ricmrs@users.noreply.github.com>
2023-02-25 16:24:04 -0300

    Update fetch api URL

--- src/lib/load-notifications.ts
+++ src/lib/load-notifications.ts
@@ -1,7 +1,7 @@
 import { INotification } from "@/interfaces/INotification"
 
 export async function loadNotifications() {
-  const res = await fetch('http://localhost:3000/api/notifications')
+  const res = await fetch('https://notification-page-ten.vercel.app/api/notifications')
   const data: INotification[] = await res.json()
 
   return data
