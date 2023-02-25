41c7cf3ad1f2f103d4db4715ea78a6deb1aa5408
Ricardo <83619661+ricmrs@users.noreply.github.com>
2023-02-25 15:58:46 -0300

    README update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,11 @@ import { INotification } from '@/interfaces/INotification';
 import Notification from '@/components/Notification';
 import { useState } from 'react';
 
-export default function Home({ notifications }: { notifications: INotification[]}) {
+export default function Home({ notifications }: { notifications: INotification[] }) {
   const [numberOfNotifications, setNumberOfNotifications] = useState(7);
   const [unreadAll, setUnreadAll] = useState(true);
 
-  function markAllAsRead(){
+  function markAllAsRead() {
     setNumberOfNotifications(0);
     setUnreadAll(false);
   }
@@ -29,12 +29,12 @@ export default function Home({ notifications }: { notifications: INotification[]
         <header className={styles.header}>
           <div className={styles.wrapper}>
             <Title>Notifications</Title>
-            <NotificationBadge numberOfNotifications={numberOfNotifications}/>
+            <NotificationBadge numberOfNotifications={numberOfNotifications} />
           </div>
           <Button onClick={markAllAsRead}>Mark all as read</Button>
         </header>
         {notifications.map(notification => (
-          <Notification 
+          <Notification
             key={notification.id}
             {...notification}
             unreadAll={unreadAll}
@@ -43,6 +43,10 @@ export default function Home({ notifications }: { notifications: INotification[]
           />
         ))}
       </main>
+      <footer style={{textAlign: 'center'}}>
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="https://www.github.com/ricmrs">ricmrs</a>.
+      </footer>
     </>
   )
 }
